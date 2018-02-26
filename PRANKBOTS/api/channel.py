4877727ef810311b07c31094342f01a35ff992ca
ariyans379 <33821665+ariyans379@users.noreply.github.com>
2018-02-26 19:39:38 +0700

    channel.py

--- PRANKBOTS/api/channel.py
+++ PRANKBOTS/api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/8.0.0"
-    LA = "IOSIPAD\t7.18.0\tiPhone OS\t11.12.1"    
+    UA = "Line/1.4.17"
+    LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"    
 
     authToken = None
     mid = None
